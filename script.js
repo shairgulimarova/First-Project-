ea4dffc93c096b465e30d40b49a07ab720666bf0
Shairgul Imarova <87937836+shairgulimarova@users.noreply.github.com>
2022-11-24 08:48:42 +0000

    Improved the styles

--- script.js
+++ script.js
@@ -8,45 +8,45 @@ const fields = document.querySelectorAll('.fields');
 
 
 
-function removeValidation() {
-  var error = document.querySelectorAll(".error");
-  for (let i = 0; i < error.length; i++) {
-    error[i].remove();
-  }
-}
+// function removeValidation() {
+//   var error = document.querySelectorAll(".error");
+//   for (let i = 0; i < error.length; i++) {
+//     error[i].remove();
+//   }
+// }
 
-function generateError(text) {
-  let error = document.createElement('div');
-  error.className = "error";
-  error.style.color = 'red';
-  error.innerHTML = text;
-  return error;
-}
+// function generateError(text) {
+//   let error = document.createElement('div');
+//   error.className = "error";
+//   error.style.color = 'red';
+//   error.innerHTML = text;
+//   return error;
+// }
 
-function checkFieldsPresence() {
-  for (let i = 0; i < fields.length; i++) {
-    if(!fields[i].value){
-      var error = generateError("Fill in the field");
-    fields[i].parentElement.insertBefore(error, fields[i])
-    }    
-  }
-}
-
-
-function reload_interval(time){
-  setTimeout(function(){
-    location.reload()
-  }, time)
-}
+// function checkFieldsPresence() {
+//   for (let i = 0; i < fields.length; i++) {
+//     if(!fields[i].value){
+//       var error = generateError("Fill in the field");
+//     fields[i].parentElement.insertBefore(error, fields[i])
+//     }    
+//   }
+// }
 
 
-myForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-  removeValidation();
-  checkFieldsPresence(); 
-  //reload_interval(2000); 
-  //console.log(event);  
-})
+// function reload_interval(time){
+//   setTimeout(function(){
+//     location.reload()
+//   }, time)
+// }
+
+
+// myForm.addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   removeValidation();
+//   checkFieldsPresence(); 
+//   //reload_interval(2000); 
+//   //console.log(event);  
+// })
 
 
 
